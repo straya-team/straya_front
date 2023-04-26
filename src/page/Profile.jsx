b932747d21166624cd03a26f1f8de6b62eb8497b
amine21345 <medaminemehrez123@gmail.com>
2023-04-27 00:27:22 +0100

    last updates

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -24,7 +24,6 @@ const Profile = (props) => {
     <div>
       <NavBar />
       <div className="empty"></div>
-
       <div className="profile">
         <img src={image ? image : profilePic} alt="" className="profilePic" />
         <p>{username ? username : "user name"}</p>
